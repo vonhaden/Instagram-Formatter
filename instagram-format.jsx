d65779a4485d06650d91f60bbb7df1dc752da09c
Andrew Von Haden <andrewvonhaden@gmail.com>
2020-11-17 13:30:37 -0600

    Updated the script so that cropped images remain open and are editable after running.

--- instagram-format.jsx
+++ instagram-format.jsx
@@ -1,88 +1,164 @@
-// Main Function
-function main(){
-    // Run Image Formatter
-    var docRef =  app.activeDocument;
+// =============================================================================
+// Instagram Format Photoshop Script
+// =============================================================================
+//
+// Description: Takes an image and reformats it for Instagram in Photoshop
+//
+// Credits
+// Author: Andrew Von Haden
+// Website: andrewvonhaden.com
+//
+// Requirements: Adobe Photoshop CC or higher
 
-    // Duplicate the document
-    docRef.duplicate(docRef.name);
-    docRef.duplicate(docRef.name);
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//  Global Variables
+////////////////////////////////////////////////////////////////////////////////
+
+// Background Color
+var bgColor = new SolidColor();
+bgColor.rgb.hexValue = 'ffffff';     // Change to hex value for any color to change BG Color.
+
+// File Resolution
+var squareResolution = 1500;        // Change this number if you want to adjust the square resolution
+
+// Suffixes
+var fullSuffix = 'Full';            // Suffix for image with pillars
+var squareSuffix = 'Square';        // Suffix for image with square crop
+var storySuffix = 'Story';          // Suffix for image with 9x16 crop
+var separator = ' - '               // separator between file name and suffix
+
+// Base Document
+var baseDoc =  app.activeDocument;
+var baseName = baseDoc.name;
+var baseTitle = baseName.substring(0, baseName.lastIndexOf('.'));
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Main Function
+////////////////////////////////////////////////////////////////////////////////
+
+function main(){
+    // Duplicate the documents
+    // Create the Full Image
+    baseDoc.duplicate(baseTitle + separator + fullSuffix);
+    // Create the Square Image
+    baseDoc.duplicate(baseTitle + separator + squareSuffix);
+    // Create the Story Image
+    baseDoc.duplicate(baseTitle + separator + storySuffix);
+
+    // Switch active document
+    app.activeDocument = app.documents.getByName(baseTitle + separator + fullSuffix);
 
     // Format full image
     formatImageFull();
 
+    // Switch active document
+    app.activeDocument = app.documents.getByName(baseTitle + separator + squareSuffix);
+
     // Format square image
     formatImageSquare();
 
+    // Switch active document
+    app.activeDocument = app.documents.getByName(baseTitle + separator + storySuffix);
+
     // Format story image
     formatImageStory();
+
+    // Close the main image
+    baseDoc.close(SaveOptions.DONOTSAVECHANGES);
 }
 
 
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Image Format Functions
+////////////////////////////////////////////////////////////////////////////////
+
 // Full Image Format
 function formatImageFull() {
-
     var docRef =  app.activeDocument;
 
+    // Set height and width
     var height = docRef.height.value;
     var width = docRef.width.value;
+
 
     // Unlock background layer
     unlockLayer();
 
-
     // Add new layer
     docRef.artLayers.add();
     docRef.activeLayer.name = "BG";
+
     // Move layer back
     docRef.artLayers[1].move(docRef.artLayers[0], ElementPlacement.PLACEBEFORE);
-
 
     // Resize the document to be square
     resizeSquare(height, width);
 
-
-    // Fill layer with white
-    whiteFill();
-
+    // Fill layer with bgColor
+    app.activeDocument.selection.fill(bgColor);
 
     // Resize the Image
-    docRef.resizeImage(1500, 1500);
-
+    docRef.resizeImage(squareResolution, squareResolution);
 
     // Flatten the Image
     docRef.flatten();
 
-
     // Save File
-    var fileSuffix = "full"
-    saveImage(fileSuffix);
+    saveImage(docRef);
 
     // Close the File
     docRef.close(SaveOptions.DONOTSAVECHANGES);
 }
-
 
 
 // Square Image Format
 function formatImageSquare(){
     var docRef =  app.activeDocument;
 
+    // Save and reopen file to allow for file folder location
+    // Save File
+    saveImage(docRef);
+
+    // Close the File
+    docRef.close(SaveOptions.DONOTSAVECHANGES);
+
+    // Open Image
+    openImage(squareSuffix);
+
+    // Reset docRef
+    var docRef =  app.activeDocument;
+
+    // Set height and width
     var height = docRef.height.value;
     var width = docRef.width.value;
+
+    // Duplicate the Background
+    unlockLayer();
+    docRef.activeLayer.duplicate();
+    docRef.activeLayer.isBackgroundLayer = true;
+    // docRef.activeLayer.allLocked = true;
+    docRef.activeLayer = docRef.layers[0];
+    docRef.layers[0].name = '--- ADJUST THIS LAYER ---';
 
     // Crop the image
     cropSquare(height, width);
 
     // Resize the Image
-    docRef.resizeImage(1500, 1500);
+    docRef.resizeImage(squareResolution, squareResolution);
 
     // Save File
-    var fileSuffix = "square"
-    saveImage(fileSuffix);
+    saveImage(docRef);
 
     // Close the File
-    docRef.close(SaveOptions.DONOTSAVECHANGES);
+    // docRef.close(SaveOptions.DONOTSAVECHANGES);
 }
 
 
@@ -90,10 +166,31 @@ function formatImageSquare(){
 function formatImageStory(){
     var docRef =  app.activeDocument;
 
+    // Save and reopen file to allow for file folder location
+    // Save File
+    saveImage(docRef);
+
+    // Close the File
+    docRef.close(SaveOptions.DONOTSAVECHANGES);
+
+    // Open Image
+    openImage(storySuffix);
+
+    // Reset docRef
+    var docRef =  app.activeDocument;
+
+    // Duplicate the Background
+    unlockLayer();
+    docRef.activeLayer.duplicate();
+    docRef.activeLayer.isBackgroundLayer = true;
+    // docRef.activeLayer.allLocked = true;
+    docRef.activeLayer = docRef.layers[0];
+    docRef.layers[0].name = '--- ADJUST THIS LAYER ---';
+
     // Set aspect ratio
     var aspectRatioPhone = 16 / 9;
 
-    // Get image aspect ration
+    // Get image aspect ratio
     var height = docRef.height.value;
     var width = docRef.width.value;
     var aspectRatioImage = height / width;
@@ -113,15 +210,20 @@ function formatImageStory(){
     docRef.resizeCanvas(resizeWidth, resizeHeight);
 
     // Save File
-    var fileSuffix = "story"
-    saveImage(fileSuffix);
+    saveImage(docRef);
 
     // Close the File
-    docRef.close(SaveOptions.DONOTSAVECHANGES);
+    // docRef.close(SaveOptions.DONOTSAVECHANGES);
 }
 
 
+
+
+////////////////////////////////////////////////////////////////////////////////
 // Auxiliary functions
+////////////////////////////////////////////////////////////////////////////////
+
+// Unlock current layer
 function unlockLayer(){
     var activeLayer = app.activeDocument.activeLayer;
 
@@ -140,6 +242,7 @@ function unlockLayer(){
     }
 }
 
+// Check document format (returns: vertical, horizontal, square)
 function checkFormat(height, width) {
     if (height > width) {
         return "vertical";
@@ -150,6 +253,7 @@ function checkFormat(height, width) {
     }
 }
 
+// Change canvas size to be square (based on largest edge)
 function resizeSquare(height, width){
     var format = checkFormat(height, width);
 
@@ -160,6 +264,7 @@ function resizeSquare(height, width){
     }
 }
 
+// Crop Image to be square (based on shortest edge)
 function cropSquare(height, width){
     var format = checkFormat(height, width);
 
@@ -170,17 +275,10 @@ function cropSquare(height, width){
     }
 }
 
-function saveImage(suffix){
-    var docRef = app.activeDocument;
-
-    // Get document name without extension
-    var docName = docRef.name.substring(0, docRef.name.lastIndexOf('.'));
-
-    // Set document name
-    var filename = docName + ' - ' + suffix;
-
+// Save the image
+function saveImage(file){
     // Savefile name with Path
-    var saveName = new File(decodeURI(app.documents[0].path) + '/' + filename + '.jpg');
+    var saveName = new File(decodeURI(baseDoc.path) + '/' + file.name);
 
     // Save Options
     var jpgSaveOptions = new JPEGSaveOptions();
@@ -189,22 +287,28 @@ function saveImage(suffix){
     jpgSaveOptions.matte = MatteType.NONE;
     jpgSaveOptions.quality = 12;
 
-    docRef.saveAs(saveName, jpgSaveOptions, true, Extension.LOWERCASE)
+    file.saveAs(saveName, jpgSaveOptions, true, Extension.LOWERCASE)
 
 }
 
-function whiteFill(){
-    var white = new SolidColor();
+// Open an Image based on suffix
+function openImage(suffix){
+    // Set filename
+    var filename = baseTitle + ' - ' + suffix + '.jpg';
 
-    white.rgb.red = 255;
-    white.rgb.blue = 255;
-    white.rgb.green = 255;
+    // Savefile name with Path
+    var filePath = new File(decodeURI(baseDoc.path) + '/' + filename );
 
-    app.activeDocument.selection.fill(white);
+    // Open Image
+    app.open(filePath);
 }
 
 
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Run the Function
+////////////////////////////////////////////////////////////////////////////////
 
 // Run the main function
 main();
